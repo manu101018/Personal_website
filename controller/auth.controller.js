ceb954b0ee06fd9745d7558be05bd5b613ebab22
Manjeet Singh <manjeetsinghmzn2002@gmail.com>
2022-05-04 12:52:48 +0530

    devolping

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,13 +2,12 @@ const User = require("../model/user.model");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
-require('dotenv').config();
+require("dotenv").config();
 
 const transport = nodemailer.createTransport(
   sendgridTransport({
     auth: {
-      api_key:
-        `${process.env.SENDGRID_API}`,
+      api_key: `${process.env.SENDGRID_API}`,
     },
   })
 );
@@ -34,15 +33,54 @@ exports.postSignup = (req, res, next) => {
       return user.save();
     })
     .then((result) => {
-      res.status(200).json({ message: "Successfully signuped!", data: [result] });
+      res
+        .status(201)
+        .json({ message: "Successfully signuped!", data: [result] });
       return transport.sendMail({
         to: email,
         from: "manjeetkakran66@gmail.com",
         subject: "Signup succeeded!",
-        html: "<h1>You successfully signed up in E-shop of Manjeet singh!<h1>",
+        html: "<h1>You successfully signed up in the world of Manjeet singh!<h1>",
       });
     })
-    .catch((err) =>{
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getLogin = (req, res, next) => {
+  res.status(200).json({ message: "Please Login to continue!" });
+};
+
+exports.postLogin = (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .json({ messsage: "No user exists, please signup first!" });
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            return res.status(200).json({
+              message: "You have successfully logined!",
+              UserDetails: {
+                Name: user.name,
+                Email: user.email,
+                Contact: user.phone,
+              },
+            });
+          }
+          return res.status(401).json({ message: "Wrong password" });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .catch((err) => {
       console.log(err);
     });
 };
--- router/auth.router.js
+++ router/auth.router.js
@@ -7,4 +7,8 @@ router.get('/signup',authController.getSignup);
 
 router.post('/signup',authController.postSignup);
 
+router.get('/login',authController.getLogin);
+
+router.post('/login',authController.postLogin);
+
 module.exports = router;
