ad279b0b5756e8d316fad02c400bff359287c914
Manjeet Singh <manjeetsinghmzn2002@gmail.com>
2022-07-01 11:14:55 +0530

    big edit

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const indexRoute = require('./router/index.router');
-const authRoute = require('./router/auth.router');
 require("dotenv").config();
  
 const app = express();
@@ -12,12 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/',indexRoute);
-app.use('/auth',authRoute);
 
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tnnqc.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
-).then(result=>{
-    console.log('Connected to Database');
-    app.listen(process.env.PORT_NAME);
+app.listen(process.env.PORT_NAME,()=>{
     console.log(`Port started at http://localhost:${process.env.PORT_NAME}`);
 });
